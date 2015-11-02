79a6b96e3d337e0a57efcc1a56d44fec98cffdd9
readuni1 <Anthony.Worrall@Reading.ac.uk>
2015-11-02 12:11:55 +0000

    Update init.pp
    changed params to rpcbind::params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class rpcbind inherits params {
+class rpcbind inherits rpcbind::params {
    package { $rpcbind::params::rpcbind_package: ensure => latest }
    service { $rpcbind::params::rpcbind_service: ensure => running, enable => true, require => Package[$rpcbind::params::rpcbind_package] }
 }

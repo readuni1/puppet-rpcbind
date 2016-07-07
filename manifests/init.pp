1cd1778f009ef4e37ad56ae74ecd092e15348dea
sssadw <anthony.worrall@reading.ac.uk>
2016-07-07 13:10:31 +0100

    added serice provider to work around service restart on RedHat 7

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
 class rpcbind {
 include rpcbind::params
-   package { $::rpcbind::params::rpcbind_package: ensure => latest }
-   service { $::rpcbind::params::rpcbind_service: ensure => running, enable => true, require => Package[$::rpcbind::params::rpcbind_package] }
+  package { $::rpcbind::params::rpcbind_package: ensure => latest }
+  service { $::rpcbind::params::rpcbind_service:
+    ensure   => running,
+    enable   => true,
+    provider => $::rpcbind::params::rpcbind_service_provider,
+    require  => Package[$::rpcbind::params::rpcbind_package]
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,45 +1,50 @@
 class rpcbind::params {
-   case $::osfamily {
-      'RedHat': {
-           case $operatingsystem {
-               'Fedora': {
-                   $rpcbind_package = "rpcbind"
-                   $rpcbind_service = "rpcbind"
-               }
-               default: {
-                   if (versioncmp($operatingsystemrelease,'6') < 0) {
-                       $rpcbind_package = "portmap"
-                       $rpcbind_service = "portmap"
-                   } else {
-                       $rpcbind_package = "rpcbind"
-                       $rpcbind_service = "rpcbind"
-                   }
-               }
-           }
-       }
-      'Suse': {
-           case $::operatingsystem {
-               'OpenSuSE': {
-                   $rpcbind_package = "rpcbind"
-                   $rpcbind_service = "rpcbind"
-               }
-               default: {	# SLE[SD]
-                   if (versioncmp($::lsbmajdistrelease,'11') < 0) {
-                       $rpcbind_package = "portmap"
-                       $rpcbind_service = "portmap"
-                   } else {
-                       $rpcbind_package = "rpcbind"
-                       $rpcbind_service = "rpcbind"
-                   }
-               }
-           }
-       }
-       'Debian': {
-           $rpcbind_package = "rpcbind"
-           $rpcbind_service = "rpcbind"
-       }
-       default: {
-           fail("rpcbind supports osfamilies RedHat, Suse, and Debian. Detected osfamily is ${::osfamily}")
-       }
-   }
+  case $::osfamily {
+    'RedHat': {
+      case $operatingsystem {
+        'Fedora': {
+          $rpcbind_package = "rpcbind"
+          $rpcbind_service = "rpcbind"
+          $rpcbind_service_provider = "default"
+        }
+        default: {
+          if (versioncmp($operatingsystemrelease,'6') < 0) {
+            $rpcbind_package = "portmap"
+            $rpcbind_service = "portmap"
+            $rpcbind_service_provider = "default"
+          } else {
+            $rpcbind_package = "rpcbind"
+            $rpcbind_service = "rpcbind"
+            $rpcbind_service_provider = "init"
+          }
+        }
+      }
+    }
+    'Suse': {
+      $rpcbind_service_provider = "default"
+      case $::operatingsystem {
+        'OpenSuSE': {
+          $rpcbind_package = "rpcbind"
+          $rpcbind_service = "rpcbind"
+        }
+        default: {	# SLE[SD]
+          if (versioncmp($::lsbmajdistrelease,'11') < 0) {
+            $rpcbind_package = "portmap"
+            $rpcbind_service = "portmap"
+          } else {
+            $rpcbind_package = "rpcbind"
+            $rpcbind_service = "rpcbind"
+          }
+        }
+      }
+    }
+    'Debian': {
+      $rpcbind_service_provider = "default"
+      $rpcbind_package = "rpcbind"
+      $rpcbind_service = "rpcbind"
+    }
+    default: {
+      fail("rpcbind supports osfamilies RedHat, Suse, and Debian. Detected osfamily is ${::osfamily}")
+    }
+  }
 }
